00984d72c5d0256e1664bec229c51e53e80f87dc
VishankSingh <68466318+VishankSingh@users.noreply.github.com>
2020-12-02 17:15:53 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -93,7 +93,7 @@ def bag_of_words(s, words):
 
 def chat():
     print("Start talking with the bot (type quit to stop and retrain to train again)!")
-        while True:
+    while True:
             inp = input("You: ")
             if inp.lower() == "/quit":
                 break
