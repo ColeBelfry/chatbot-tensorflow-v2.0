6d1fed0355cdf31dc1f3c8f02e0013638786ac94
VishankSingh <68466318+VishankSingh@users.noreply.github.com>
2020-12-04 18:47:13 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -8,3 +8,115 @@ import tensorflow as tf
 from tensorflow import keras 
 import random
 import json
+
+with open("intents.json") as file:
+    data = json.load(file)
+    
+words = []
+labels = []
+docs_x = []
+docs_y = []
+
+for intent in data["intents"]:
+    for pattern in intent["patterns"]:
+        wrds = nltk.word_tokenize(pattern)
+        words.extend(wrds)
+        docs_x.append(wrds)
+        docs_y.append(intent["tag"])
+
+    if intent["tag"] not in labels:
+        labels.append(intent["tag"])
+
+words = [stemmer.stem(w.lower()) for w in words if w != ("?" or "!")]
+words = sorted(list(set(words)))
+
+labels = sorted(labels)
+
+training = []
+output = []
+
+out_empty = [0 for _ in range(len(labels))]
+
+for x, doc in enumerate(docs_x):
+    bag = []
+
+    wrds = [stemmer.stem(w.lower()) for w in doc]
+
+    for w in words:
+        if w in wrds:
+            bag.append(1)
+        else:
+            bag.append(0)
+
+    output_row = out_empty[:]
+    output_row[labels.index(docs_y[x])] = 1
+
+    training.append(bag)
+    output.append(output_row)
+
+
+training = numpy.array(training)
+output = numpy.array(output)
+
+model = tf.keras.Sequential()
+
+model.add(tf.keras.layers.InputLayer(input_shape=(len(training[0]))))
+model.add(tf.keras.layers.Dense(8))
+model.add(tf.keras.layers.Dense(8))
+model.add(tf.keras.layers.Dense(8))
+model.add(tf.keras.layers.Dense(len(output[0]), activation="softmax"))
+
+def train():
+    model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
+    model.fit(training, output, epochs=500, batch_size=256)
+    model.save('model.h5')
+
+try:
+    model = keras.models.load_model('model.h5')
+except:
+    train()
+    
+def bag_of_words(s, words):
+    bag = [0 for _ in range(len(words))]
+
+    s_words = nltk.word_tokenize(s)
+    s_words = [stemmer.stem(word.lower()) for word in s_words]
+
+    for se in s_words:
+        for i, w in enumerate(words):
+            if w == se:
+                bag[i] = 1
+                
+    return numpy.array([bag])
+  
+def chat():
+        print("Start talking with the bot (type quit to stop and retrain to train again)!")
+        while True:
+            inp = input("You: ")
+            if inp.lower() == "/quit":
+                break
+                exit()
+            elif inp.lower() == "/retrain":
+                train()
+                chat()
+            else:
+                results = model.predict([bag_of_words(inp, words)])[0]
+               
+                results_index = numpy.argmax(results)
+                tag = labels[results_index]
+
+                if results[results_index] > 0.9:
+                    for tg in data["intents"]:
+                        if tg["tag"] == tag:
+                            responses = tg["responses"]
+
+                    print(f"{random.choice(responses)}   (Category: {tag})")
+
+                else:
+                    print("Please rephrase it!") 
+                    with open('exception.txt', 'r') as f:
+                        if inp not in f.read():
+                            with open('exception.txt', 'a') as f:
+                                f.write(f'{inp}  (Predicted category: {tag})\n')
+                                
+chat()
